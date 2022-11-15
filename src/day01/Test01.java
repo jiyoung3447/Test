2927ade619182e4afab7af08e9ca94e1e2d9eab2
Jiyoung Park <31343447@naver.com>
2022-11-15 19:49:41 +0900

    테스트1

---
+++ src/day01/Test01.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		int n =49;
+		String str = null;
+		if ( ++n >= 50) {
+			str = "50보다 크다";
+		} 
+		else {
+		    str = "50보다 작다";	
+		}
+		System.out.println(str);
+	}
+
+}
